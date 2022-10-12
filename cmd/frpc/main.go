6a575a8c6a29141687b976604c8655a03c24bade
chrelyonly <1172576293@qq.com>
2022-10-12 16:50:41 +0800

    1

--- cmd/frpc/main.go
+++ cmd/frpc/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"github.com/fatedier/frp/cmd/frpc/controller"
+	"encoding/json"
 	"github.com/fatedier/frp/cmd/frpc/sub"
 	"github.com/fatedier/frp/pkg/util/log"
+	"io/ioutil"
 	"net/http"
 	"sync"
 )
@@ -11,27 +12,51 @@ import (
 var waitGroup sync.WaitGroup
 
 func main() {
+	log.Info("********************初始化配置中********************")
 	log.Info("开始连接服务器")
 	log.Info("检测版本状态")
 	log.Info("等待服务器响应")
-	get, err := http.Get("https://chrelyonly.cn/blog/blogconfig/blogConfig")
+	get, err := http.Get("https://chrelyonly.cn/blog/frpApi/checkVersion")
 	if err != nil {
 		log.Error("链接服务器超时")
 	}
 	status := get.StatusCode
 	if status != 200 {
-		log.Error("链接服务器超时")
+		log.Info("链接服务器超时")
 	}
-	err = get.Body.Close()
+	body, err := ioutil.ReadAll(get.Body)
 	if err != nil {
-		log.Error("关闭错误")
+		log.Info("读取错误")
 		return
 	}
+	//判断版本
+	var res R
+	_ = json.Unmarshal(body, &res)
+	if res.Code == 200 {
+		version := "1.0"
+		log.Info("本地frp管理版本: " + version)
+		log.Info("远端frp管理版本: " + res.Msg)
+		if version == res.Msg {
+			log.Info("********************当前是最新版本,无需更新********************")
+		} else {
+			log.Info("********************发现新版本请更新********************")
+		}
+	} else {
+		log.Info("********************获取版本时异常********************")
+		log.Info("********************" + res.Msg + "!!!********************")
+	}
 	waitGroup.Add(2)
-	go controller.InitWeb()
+	//执行完之后启动web页面
+	//go controller.InitWeb()
 	go sub.Execute()
 	waitGroup.Wait()
 	log.Info("系统系统完成")
-	//执行完之后启动web页面
+}
 
+// R 转换结构
+type R struct {
+	Code    int32             `json:"code"`
+	Data    map[string]string `json:"data"`
+	Success bool              `json:"success"`
+	Msg     string            `json:"msg"`
 }
